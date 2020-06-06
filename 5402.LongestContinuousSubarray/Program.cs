50c55a46cc26e8450ccc1e13594af4b300da6b7d
Chung Nguyen <chungdinhnguyen.ukb@gmail.com>
2020-06-06 14:43:15 +0700

    no message

---
+++ 5402.LongestContinuousSubarray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _5402.LongestContinuousSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] {4, 2, 2, 2, 4, 4, 2, 2};
+            int limit = 0;
+            var result = LongestSubarray(nums, limit);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        public static int LongestSubarray(int[] nums, int limit)
+        {
+            int currentSize = 0, min , max;
+            int i = 0, j = 0, n = nums.Length;
+
+            while (i < n && j < n)
+            {
+                min = int.MaxValue;
+                max = int.MinValue;
+
+                for (int k = i; k <= j; k++)
+                {
+                    if (nums[k] < min)
+                    {
+                        min = nums[k];
+                    }
+
+                    if (nums[k] > max)
+                    {
+                        max = nums[k];
+                    }
+                }
+
+                if (max - min <= limit)
+                {
+                    currentSize = Math.Max(currentSize, j - i + 1);
+                    j++;
+
+                    continue;
+                }
+
+                i++;
+            }
+
+            return currentSize;
+        }
+    }
+}
